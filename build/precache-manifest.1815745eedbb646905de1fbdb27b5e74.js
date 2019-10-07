08dd07e69a7707d9fc0a219ce869fc7ba711100d
Mayuresh Bhardwaj <maybhard@publicisgroupe.net>
2019-10-07 10:54:21 -0400

    adding files

---
+++ build/precache-manifest.1815745eedbb646905de1fbdb27b5e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811ed378771cfd964d1798c4a3c957e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf4410233bbd10615e7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e286183fff7b1c1e88f3",
+    "url": "/static/js/2.14750350.chunk.js"
+  },
+  {
+    "revision": "4cf4410233bbd10615e7",
+    "url": "/static/js/main.c3c3898a.chunk.js"
+  },
+  {
+    "revision": "da09d83c8301d27a53cc",
+    "url": "/static/js/runtime-main.7f265172.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
