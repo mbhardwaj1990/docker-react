843fed695386ad8675ad35953cdc1e0433d331b2
Mayuresh Bhardwaj <maybhard@publicisgroupe.net>
2019-10-09 11:44:35 -0400

    Adding some new Featcher

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn this awesome language.
+          improving myself 
         </a>
       </header>
     </div>
